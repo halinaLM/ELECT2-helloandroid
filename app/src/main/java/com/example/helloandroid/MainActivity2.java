bc79aefbf7a4be08f8562f0352f517897701659e
Halina, Lorry Mae <halinalorr19@gmail.com>
2023-02-18 13:17:25 +0800

    ACTIVITY2 02/18/2023

--- app/src/main/java/com/example/helloandroid/MainActivity2.java
+++ app/src/main/java/com/example/helloandroid/MainActivity2.java
@@ -2,7 +2,11 @@ package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Button button = (Button) findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
