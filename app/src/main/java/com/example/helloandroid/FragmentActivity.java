02fada7aa73f77ea0758478b4d7c2ccb0042a836
Princess Halina <halinalorr19@gmail.com>
2023-03-15 12:19:02 +0800

    lorry

---
+++ app/src/main/java/com/example/helloandroid/FragmentActivity.java
@@ -0,0 +1,48 @@
+package com.example.helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Fragment;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FragmentActivity extends AppCompatActivity {
+
+    Button firstfragment, secondfragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+        firstfragment = (Button) findViewById(R.id.firstfragment);
+        secondfragment = (Button) findViewById(R.id.secondfragment);
+        firstfragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FirstFragment());
+            }
+        });
+
+        secondfragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new SecondFragment());
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment){
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.framelayout, fragment);
+        fragmentTransaction.commit();
+
+    }
+
+//
+
+
+
+}
