8e0d6298fd6bdb34eb09b221e82ebd49f88f9165
Admin <halinalorr19@gmail.com>
2023-03-18 14:34:38 +0800

    actionnbar 03/18/2023

---
+++ app/actionbar2/src/main/java/com/example/actionbar2/FragDialog.java
@@ -0,0 +1,19 @@
+package com.example.actionbar2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+
+public class FragDialog extends androidx.fragment.app.DialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_frag_dialog, container, false);
+    }
+}
--- app/actionbar2/src/main/java/com/example/actionbar2/actionbar.java
+++ app/actionbar2/src/main/java/com/example/actionbar2/actionbar.java
@@ -1,10 +1,17 @@
 package com.example.actionbar2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ActionBar;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+
 
 public class actionbar extends AppCompatActivity {
 
@@ -13,14 +20,48 @@ public class actionbar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actionbar);
 
-        ActionBar actionBar = getActionBar();
-
-        actionBar.setTitle(" Gfg | ActionBar");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(" GFG | ActionBar");
         actionBar.setSubtitle("This is an action bar");
-        actionBar.setSubtitle(R.drawable.ic_launcher_background);
-        actionBar.setDisplayUseLogoEnabled(true);
-        actionBar.setDisplayHomeAsUpEnabled();
+        actionBar.setIcon(R.mipmap.ic_launcher);
+
     }
-    //@Override
-   // public boolean onOptionsItemSelected(Menu menu)
+    @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search:
+                showAlertDialog();
+                break;
+            case R.id.refresh:
+                FragDialog dialogFragment = new FragDialog();
+                dialogFragment.show(getSupportFragmentManager(), "My fragment");
+                break;
+            case R.id.copy:
+                Toast.makeText(this, "Copied", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showAlertDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alert!").setMessage("Danger! ! ! You are failing!");
+
+        builder.setPositiveButton("Okay!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 }
